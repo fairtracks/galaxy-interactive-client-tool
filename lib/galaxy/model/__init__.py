5cbbbf3626f99e19cf50b6eb9ac98d43f651c055
Sveinung Gundersen <sveinugu@gmail.com>
2023-08-27 23:50:18 +0200

    Added 'label' column to 'interactivetool_entry_point' in db, parser, and api

--- lib/galaxy/model/__init__.py
+++ lib/galaxy/model/__init__.py
@@ -2676,12 +2676,14 @@ class InteractiveToolEntryPoint(Base, Dictifiable, RepresentById):
     deleted = Column(Boolean, default=False)
     created_time = Column(DateTime, default=now)
     modified_time = Column(DateTime, default=now, onupdate=now)
+    label = Column(TEXT)
     job = relationship("Job", back_populates="interactivetool_entry_points", uselist=False)
 
     dict_collection_visible_keys = [
         "id",
         "job_id",
         "name",
+        "label",
         "active",
         "created_time",
         "modified_time",
@@ -2691,6 +2693,7 @@ class InteractiveToolEntryPoint(Base, Dictifiable, RepresentById):
         "id",
         "job_id",
         "name",
+        "label",
         "active",
         "created_time",
         "modified_time",
